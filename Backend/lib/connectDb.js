fdb00d0da2a672d889ede1cab19b3310cbee3e1b
Sufyan Liaqat <sufyanliaquat58@gmail.com>
2025-06-17 01:23:59 +0500

    `Removed console.log statement from MongoDB connection attempt`

--- Backend/lib/connectDb.js
+++ Backend/lib/connectDb.js
@@ -4,7 +4,7 @@ dotenv.config();
 
 const connectDb = async () => {
     try {
-        const conn = await mongoose.connect(process.env.MONGODB_URI);
+        await mongoose.connect(process.env.MONGODB_URI);
         console.log("Connected to MongoDB:");
         
     } catch (error) {
